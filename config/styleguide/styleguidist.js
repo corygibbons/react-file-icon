20131ea728e39008f65358233597bcd4c3e848d6
Cory Gibbons <hello@corygibbons.com>
2018-02-07 08:43:42 -0800

    Add GitHub banner to styleguide

--- config/styleguide/styleguidist.js
+++ config/styleguide/styleguidist.js
@@ -1,12 +1,13 @@
 const { createConfig, babel } = require('webpack-blocks');
 
 module.exports = {
-  components: '../src/components/*.js',
+  components: '../../src/components/*.js',
   serverPort: 8080,
   showSidebar: false,
   showUsage: true,
   skipComponentsWithoutExample: true,
-  styleguideDir: '../public',
+  styleguideDir: '../../public',
+  template: './index.html',
   title: 'React File Icon',
   webpackConfig: createConfig([babel()])
 };
