b5ce78a9cf3ed7846d7029b75f0eb6529d5a4c4a
Cory Gibbons <hello@corygibbons.com>
2020-07-04 15:48:21 -0400

    Build with microbundle

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import FileIcon from './FileIcon';
-
-export default FileIcon;
-export { default as defaultStyles } from './defaultStyles/index';
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as FileIcon } from "./components/FileIcon";
+export { default as defaultStyles } from "./components/defaultStyles/index";
