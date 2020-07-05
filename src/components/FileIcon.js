03a5ea6f84c16cfdd205976293ec02f0e8da910b
njuro <njuro@users.noreply.github.com>
2020-07-05 14:23:43 +0200

    Add xmlns definition to FileIcon (#16)

--- src/components/FileIcon.js
+++ src/components/FileIcon.js
@@ -90,6 +90,7 @@ export const FileIcon = ({
 
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       viewBox={`0 0 ${VIEWBOX.WIDTH} ${VIEWBOX.HEIGHT}`}
       width={size}
       height={size}
